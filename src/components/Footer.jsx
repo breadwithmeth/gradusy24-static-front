5d8e9c21978c47031dceb3872ab62d0c80a87e5b
shrvse <sergeq2222@gmail.com>
2026-06-09 00:12:34 +0500

    +

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function Footer() {
   return (
     <footer id="contacts" className="site-footer">
       <div className="container footer-inner">
         <span>Градусы24</span>
         <span>21+</span>
-        <a href="#">Политика конфиденциальности</a>
-        <a href="#">Пользовательское соглашение</a>
+        <Link to="/privacy">Политика конфиденциальности</Link>
+        <Link to="/offer">Пользовательское соглашение</Link>
         <a href="mailto:contact@gradusy24.example">Контакты</a>
       </div>
     </footer>
